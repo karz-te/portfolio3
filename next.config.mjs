b6fd6956ee42157858ff201d1a66e4fc474b6223
Teruhito Kawamura <tdwtr720@ybb.ne.jp>
2024-11-22 13:29:02 +0900

    correction3

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
 //*@type {import('next').NextConfig}*/
+import { writeFileSync } from 'fs';
 
 export default {
     output: 'export',
     basePath: '/portfolio3', // デプロイ先リポジトリ名が 'portfolio3' の場合
     assetPrefix: '/portfolio3/',
     distDir: 'docs',
+    async exportPathMap(defaultPathMap, { outDir }) {
+        writeFileSync(`${outDir}/.nojekyll`, '');
+        return defaultPathMap;
+    }, 
 };
 
 /*
