49a7fef7b24b4dd2cb54fd3253cea09f541edee9
Teruhito Kawamura <tdwtr720@ybb.ne.jp>
2024-11-23 20:51:17 +0900

    Update next.config.mjs for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -9,9 +9,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default {
     output: 'export',
-    basePath:'/portfolio3', // デプロイ先リポジトリ名が 'portfolio3' の場合
-    assetPrefix:'/portfolio3/',
-    distDir: 'docs',
+    //basePath:'/portfolio3', // デプロイ先リポジトリ名が 'portfolio3' の場合
+    //assetPrefix:'/portfolio3/',
+    //distDir: 'docs',
     trailingSlash: true,
     publicRuntimeConfig: {
         basePath: '/portfolio3', // ランタイムで利用可能な basePath
