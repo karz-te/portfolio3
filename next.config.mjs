2e693c65afb355c3a8bc76c9abf11ae68efd49c9
Teruhito Kawamura <tdwtr720@ybb.ne.jp>
2024-11-23 14:11:34 +0900

    correction7

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,11 @@
 
 // GitHub Pages用
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default {
     output: 'export',
     basePath:'/portfolio3', // デプロイ先リポジトリ名が 'portfolio3' の場合
